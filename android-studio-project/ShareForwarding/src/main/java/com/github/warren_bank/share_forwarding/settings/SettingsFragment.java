30258e6ed12635861fec44b88317796f5ca2be5a
Warren R. Bank <warren.r.bank@gmail.com>
2026-02-04 15:26:39 -0800

    v1.2.2: [Android 15+] fix edge-to-edge layout enforcement
    see:
    * https://developer.android.com/about/versions/15/behavior-changes-15#edge-to-edge
    * https://medium.com/androiddevelopers/insets-handling-tips-for-android-15s-edge-to-edge-enforcement-872774e8839b
    
    docs:
    * https://developer.android.com/reference/android/view/View#attr_android:fitsSystemWindows
    * https://developer.android.com/reference/android/view/View#setFitsSystemWindows(boolean)

--- android-studio-project/ShareForwarding/src/main/java/com/github/warren_bank/share_forwarding/settings/SettingsFragment.java
+++ android-studio-project/ShareForwarding/src/main/java/com/github/warren_bank/share_forwarding/settings/SettingsFragment.java
@@ -2,13 +2,28 @@ package com.github.warren_bank.share_forwarding.settings;
 
 import com.github.warren_bank.share_forwarding.R;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingsFragment extends PreferenceFragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     addPreferencesFromResource(R.xml.preferences);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+
+    // fix for Android 15+ edge-to-edge layout enforcement
+    if ((view != null) && (Build.VERSION.SDK_INT >= 14))
+      view.setFitsSystemWindows(true);
+
+    return view;
   }
 }
